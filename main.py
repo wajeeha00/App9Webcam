cb7f8d38179c6c83317a75fa3f13f5a72314c959
wajeeha00 <wajeehaaftab7890@gmail.com>
2024-07-25 18:49:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+from emailing import send_email
 import cv2
 import time 
+import glob
 video = cv2.VideoCapture(0)
 time.sleep(1)
 first_frame = None
-
+status_list = []
+count = 1
 while True:
+   status =0
    check, frame = video.read()
+   
    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
    gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21),0)
    
@@ -26,7 +31,20 @@ while True:
       if cv2.contourArea(contour) < 5000:
          continue
       (x, y, w, h) = cv2.boundingRect(contour)
-      cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+      rectangle=cv2.rectangle(frame, (x, y), 
+                              (x+w, y+h), (0, 255, 0), 3)
+      if rectangle.any():
+         status=1
+         cv2.imwrite(f"images/{count}.png",frame)
+         count = count + 1
+         all_images = glob.glob("images/*.png")
+         index= int(len(all_images)/2)
+         image_with_object = all_images[index]
+   status_list.append(status)
+   status_list=status_list[-2:]
+   if status_list[0] == 1 and status_list[1] == 0:
+      send_email(image_with_object)
+      
    cv2.imshow("Capturing", frame)
    key = cv2.waitKey(1)
    if key == ord('q'):
